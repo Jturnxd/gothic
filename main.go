519c466f73268d92654725c5742b187b186c8765
Jturnxd <jturn1@yandex.ru>
2022-07-05 01:18:16 +0300

    add basic functionality

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"time"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+	"github.com/pquerna/otp/totp"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	a := app.New()
+	w := a.NewWindow("gothic")
+
+	hello := widget.NewLabel("Welcome to gothic, enter your secret key and press the button below to generate the code.")
+	secret := widget.NewEntry()
+
+	w.SetContent(container.NewVBox(
+		hello,
+		secret,
+		widget.NewButton("Generate code", func() {
+			code, err := totp.GenerateCode(secret.Text, time.Now())
+			if err != nil {
+				log.Println(err)
+			} else {
+				hello.SetText(code)
+			}
+		}),
+	))
+	w.Resize(fyne.NewSize(400, 400))
+
+	w.ShowAndRun()
 }
